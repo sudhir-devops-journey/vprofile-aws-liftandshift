8d7309b076b826e80c098872e7c5ed8a98bcd253
sallada <sallada@cisco.com>
2025-03-25 22:40:17 +0530

    removed target

--- userdata/mysql.sh
+++ userdata/mysql.sh
@@ -7,7 +7,7 @@ sudo dnf install mariadb105-server -y
 sudo systemctl start mariadb
 sudo systemctl enable mariadb
 cd /tmp/
-git clone -b main https://github.com/hkhcoder/vprofile-project.git
+git clone -b main https://github.com/sudhir-devops-journey/vprofile-aws-liftandshift.git
 #restore the dump file for the application
 sudo mysqladmin -u root password "$DATABASE_PASS"
 sudo mysql -u root -p"$DATABASE_PASS" -e "ALTER USER 'root'@'localhost' IDENTIFIED BY '$DATABASE_PASS'"
